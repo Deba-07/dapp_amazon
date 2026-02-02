bb09259f38417d122240ae766de99b5ca9077a7a
Deba-07 <dynamicdeba07@gmail.com>
2026-02-02 11:06:11 +0530

    setup ccontract and test

---
+++ test/Dappazon.js
@@ -0,0 +1,22 @@
+const { expect } = require("chai");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+describe("Dappazon", () => {
+  let dappazon;
+
+  // Deploy contract
+  beforeEach(async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", () => {
+    it("has a name", async () => {
+      const name = await dappazon.name();
+      expect(name).to.be.equal("Dappazon");
+    });
+  });
+});
